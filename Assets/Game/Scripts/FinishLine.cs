27c090e5a61f95cdf07358e44242e7affa4a45f1
Hayri Mert Kurt <hayrimertkurt@outlook.com>
2022-04-17 18:10:47 +0300

    hayri,deneme

---
+++ Assets/Game/Scripts/FinishLine.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FinishLine : MonoBehaviour
+{
+    public GameObject spoon;
+    public GameObject objectToThrow;
+    public Vector3 endPoint;
+    [Header("Jump Parameters")]
+    public float jumpDuration;
+    public float jumpPower;
+    public bool snapping = false;
+    public int numOfJumps;
+    private int count = 63;
+    private int clickCount = 0;
+    [HideInInspector]public bool
+
+
+}
