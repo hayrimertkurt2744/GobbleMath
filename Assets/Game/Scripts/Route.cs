fe2520d84cc515e662a4b845fa6cd8ab419469aa
Hayri Mert Kurt <hayrimertkurt@outlook.com>
2022-05-08 10:28:35 +0300

    bezier uı

--- Assets/Game/Scripts/Route.cs
+++ Assets/Game/Scripts/Route.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Route : MonoBehaviour
 {
     [SerializeField]
+
     private Transform[] controlPoints;
     private Vector2 gizmosPosition;
 
@@ -12,10 +13,12 @@ public class Route : MonoBehaviour
     {
         for (float t= 0; t <= 1; t+=0.05f)
         {
-            gizmosPosition = Mathf.Pow(1 - t, 1) * controlPoints[0].position + t * controlPoints[1].position;
-            Gizmos.DrawSphere(gizmosPosition, 1f);
+            gizmosPosition = Mathf.Pow(1 - t, 3) * controlPoints[0].position +3 * Mathf.Pow(1 - t, 2)*t*controlPoints[1].position+3*(1-t)*Mathf.Pow(t,2)*controlPoints[2].position+Mathf.Pow(t,3)*controlPoints[3].position;
+            Gizmos.DrawSphere(gizmosPosition, 0.25f);
         }
         Gizmos.DrawLine(new Vector2(controlPoints[0].position.x, controlPoints[0].position.y), new Vector2(controlPoints[1].position.x, controlPoints[1].position.y));
+        Gizmos.DrawLine(new Vector2(controlPoints[2].position.x, controlPoints[2].position.y), new Vector2(controlPoints[3].position.x, controlPoints[3].position.y));
+      
     }
 
 }
